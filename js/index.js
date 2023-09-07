276499278d2e4c4f36c168c0ea79ab5804912ea7
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2023-09-07 00:00:09 -0700

    added home page

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let nav = document.querySelector('#nav-area');
+let btn = document.querySelector('.toggle');
+let mask = document.querySelector('#mask');
+
+// Hambarger Menu
+btn.onclick = () => {
+    nav.classList.toggle('open');
+}
+
+mask.onclick = () => {
+    nav.classList.toggle('open'); 
+}
